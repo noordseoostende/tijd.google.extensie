0b75f25b60ec77d38d7baa3de1c4d2536559be77
noordseoostende <voropayev1@mail.ru>
2021-06-30 21:28:23 +0200

    de achtergrond aanpassen

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 const timeElement = document.getElementById("time")
+const nameElement = document.getElementById("name")
 const currentTime = new Date().toLocaleTimeString()
 timeElement.textContent = `The time is: ${currentTime}`
 
@@ -8,3 +9,8 @@ chrome.action.setBadgeText({
   console.log("Messenger bag brunch vice butcher irony VHS four dollar toast asymmetrical sustainable bitters kinfolk. Literally kombucha mustache deep v activated charcoal, yr blue bottle leggings pop-up bitters organic fam synth. ")
 }
 )
+
+chrome.storage.sync.get(["name"], (res) => {
+  const name = res.name ?? "???"
+  nameElement.textContent = `citaat van de dag is ${name}`
+})
