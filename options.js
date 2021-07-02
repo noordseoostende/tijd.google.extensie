1c686d97a1885225c1b07feeed813bdf4c843174
noordseoostende <voropayev1@mail.ru>
2021-07-02 20:42:33 +0200

    stop start reset

--- options.js
+++ options.js
@@ -1,16 +1,18 @@
 const nameInput = document.getElementById("name-input")
+const timeInput = document.getElementById("time-input")
 const saveBtn = document.getElementById("save-btn")
 
 saveBtn.addEventListener("click", () => {
   const name = nameInput.value
+  const notificationTime = timeInput.value
   chrome.storage.sync.set({
     name,
-
-  }, () => {
-    console.log(`citaat van de dag is ${name}`)
+    notificationTime,
   })
-})
+  })
 
-chrome.storage.sync.get(["name"], (res) => {
+
+chrome.storage.sync.get(["name", "notificationTime"], (res) => {
   nameInput.value = res.name ?? "hier moet jouw citaat zijn"
+  timeInput.value = res.notificationTime ?? 1000
 })
--- popup.js
+++ popup.js
@@ -21,3 +21,22 @@ chrome.storage.sync.get(["name"], (res) => {
   nameElement.textContent = `citaat van de dag is ${name}`
 })
 
+const startBtn = document.getElementById("start")
+const stopBtn = document.getElementById("stop")
+const resetBtn = document.getElementById("reset")
+
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: true,
+  })
+})
+stopBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  })
+})
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  })
+})
